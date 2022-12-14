8b3c3241e7866e3fc3cbd6191cdc50bfa6727d14
Sophie Dankel <47993817+sdankel@users.noreply.github.com>
2022-12-13 18:37:49 -0800

    Apply suggestions from code review

--- sway-lsp/src/capabilities/code_actions.rs
+++ sway-lsp/src/capabilities/code_actions.rs
@@ -14,7 +14,9 @@ pub(crate) fn code_actions(
     text_document: TextDocumentIdentifier,
     temp_uri: &Url,
 ) -> Option<CodeActionResponse> {
-    let (_, token) = session.token_at_position(temp_uri, range.start.clone())?;
+    let (_, token) = session
+        .token_map()
+        .token_at_position(temp_uri, range.start.clone())?;
     token.typed.and_then(|typed_token| {
         let maybe_decl = match typed_token {
             TypedAstToken::TypedDeclaration(decl) => Some(decl),
@@ -37,69 +39,3 @@ pub(crate) fn code_actions(
             })
     })
 }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use crate::{
-//         core::document::TextDocument,
-//         test_utils::{get_absolute_path, get_url},
-//     };
-//     use serial_test::serial;
-
-//     use tower_lsp::lsp_types::{Position, Range, TextDocumentIdentifier};
-
-//     #[test]
-//     fn code_actions_returns_none() {
-//         let session = Arc::new(Session::new());
-//         let range = Range {
-//             start: Position {
-//                 character: 0,
-//                 line: 0,
-//             },
-//             end: Position {
-//                 character: 0,
-//                 line: 0,
-//             },
-//         };
-
-//         let uri = get_url(&get_absolute_path("sway-lsp/test/fixtures/cats.txt"));
-//         let text_document = TextDocumentIdentifier { uri: uri.clone() };
-
-//         let result = code_actions(session, &range, text_document, &uri);
-//         assert!(result.is_none(), "result = {:?}", result);
-//     }
-
-//     #[tokio::test]
-//     #[serial]
-//     async fn code_actions_returns_abi_impl() {
-//         let session = Session::new();
-//         let path = get_absolute_path("sway-lsp/test/fixtures/fizzbuzz/src/main.sw");
-//         let document = TextDocument::build_from_path(&path.clone()).unwrap();
-//         let _ = Session::store_document(&session, document);
-
-//         let range = Range {
-//             start: Position {
-//                 character: 5,
-//                 line: 10,
-//             },
-//             end: Position {
-//                 character: 5,
-//                 line: 10,
-//             },
-//         };
-
-//         let main_uri = get_url(&path);
-//         let text_document_id = TextDocumentIdentifier {
-//             uri: main_uri.clone(),
-//         };
-//         let _ = session.init(&get_url(&get_absolute_path(
-//             "sway-lsp/test/fixtures/fizzbuzz/Forc.toml",
-//         )));
-
-//         eprintln!("\n\n\n\nsession.token_map {:?}", session.token_map());
-
-//         let result = code_actions(Arc::new(session), &range, text_document_id, &main_uri);
-//         assert!(result.is_some(), "result = {:?}", result);
-//     }
-// }
